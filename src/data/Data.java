f4691a866ed08560c2732b71ebd5cce10489e06c
MannyKuflik <39571252+MannyKuflik@users.noreply.github.com>
2020-05-07 17:35:04 -0400

    Add files via upload

--- src/data/Data.java
+++ src/data/Data.java
@@ -10,35 +10,131 @@ import org.jsoup.select.Elements;
 
 public class Data {
     HashMap<String, String> url;
-    
+
     private static String userStock = null;
+    private Stock uStock;
 
     public Data() {
         this.url = new HashMap<String, String>();
-        this.url.put("Large Cap", "https://www.tradingview.com/markets/stocks-usa/market-movers-large-cap/");
-        this.url.put("Gainers", "https://www.tradingview.com/markets/stocks-usa/market-movers-gainers/");
-        this.url.put("Losers", "https://www.tradingview.com/markets/stocks-usa/market-movers-losers/");
-        this.url.put("Active", "https://www.tradingview.com/markets/stocks-usa/market-movers-active/");
-        this.url.put("Volatile", "https://www.tradingview.com/markets/stocks-usa/market-movers-most-volatile/");
-        this.url.put("Overbought", "https://www.tradingview.com/markets/stocks-usa/market-movers-overbought/");
-        this.url.put("Oversold", "https://www.tradingview.com/markets/stocks-usa/market-movers-oversold/");
-    }
-    
-    public static void setStock(String ticker) {
-        userStock = ticker;
-    }
-    
-    public static String getStock() {
-        return userStock;
+        this.url.put("Default",
+                "https://www.tradingview.com/markets/stocks-usa/market-movers-large-cap/");
+        this.url.put("Large Cap",
+                "https://www.tradingview.com/markets/stocks-usa/market-movers-large-cap/");
+        this.url.put("Gainers",
+                "https://www.tradingview.com/markets/stocks-usa/market-movers-gainers/");
+        this.url.put("Losers",
+                "https://www.tradingview.com/markets/stocks-usa/market-movers-losers/");
+        this.url.put("Most Active",
+                "https://www.tradingview.com/markets/stocks-usa/market-movers-active/");
+        this.url.put("Most Volatile",
+                "https://www.tradingview.com/markets/stocks-usa/market-movers-most-volatile/");
+        this.url.put("Overbought",
+                "https://www.tradingview.com/markets/stocks-usa/market-movers-overbought/");
+        this.url.put("Oversold",
+                "https://www.tradingview.com/markets/stocks-usa/market-movers-oversold/");
+        this.url.put("Energy Minerals",
+                "https://www.tradingview.com/markets/stocks-usa/sectorandindustry-sector/energy-minerals/");
+        this.url.put("Technology",
+                "https://www.tradingview.com/markets/stocks-usa/sectorandindustry-sector/electronic-technology/");
+        this.url.put("Technology2",
+                "https://www.tradingview.com/markets/stocks-usa/sectorandindustry-sector/technology-services/");
+        this.url.put("BioTech",
+                "https://www.tradingview.com/markets/stocks-usa/sectorandindustry-sector/health-technology/");
+        this.url.put("Medicine",
+                "https://www.tradingview.com/markets/stocks-usa/sectorandindustry-sector/health-services/");
+        this.url.put("Finance",
+                "https://www.tradingview.com/markets/stocks-usa/sectorandindustry-sector/finance/");
+        this.url.put("Manufacturing",
+                "https://www.tradingview.com/markets/stocks-usa/sectorandindustry-sector/producer-manufacturing/");
+        this.url.put("Retail",
+                "https://www.tradingview.com/markets/stocks-usa/sectorandindustry-sector/retail-trade/");
     }
 
-    public ArrayList getStockData(String type) {
+    public Stock setStock(String ticker) {
+        String userUrl = "https://finance.yahoo.com/quote/" + ticker;
+        String price = "N/A", ch = "N/A", pch = "N/A", vol = "N/A", cap = "N/A",
+                p2e = "N/A", eps = "N/A";
+        try {
+            Document doc = Jsoup.connect(userUrl).get();
+            Elements links = doc.getElementsByClass("Bdc($seperatorColor)");
+            links.addAll(doc.getElementsByClass("Mend(20px)"));
+//            Elements links = doc.getElementsByClass("Trsdu(0.3s)");
+//            System.out.println("Found links: " + links.size());
+//            String[] changes = links.get(10).text().split(" ");
+//            String change = changes[0];
+//            String pChange = changes[1];
+//            uStock = new Stock(ticker, links.get(9).text(), pChange, change,
+//                    links.get(15).text(), links.get(17).text(),
+//                    links.get(19).text(), links.get(20).text());
+//            System.out.println(uStock.toString());
+//            int i = 0;
+            for (Element link : links) {
+                String curr = link.text();
+                if (curr.contains("Market Cap")) {
+//                    System.out.println(i + ": cap " + link.text());
+                    cap = curr.substring(curr.indexOf("p") + 2);
+//                    System.out.println(cap);
+                } else if (curr.contains("PE Ratio")) {
+//                    System.out.println(i + ": p2e " + link.text());
+                    p2e = curr.substring(curr.indexOf(")") + 2);
+//                    System.out.println(p2e);
+                } else if (curr.contains("EPS")) {
+//                    System.out.println(i + ": eps " + link.text());
+                    eps = curr.substring(curr.indexOf(")") + 2);
+//                    System.out.println(eps);
+                } else if (curr.contains("Volume") && !curr.contains("Avg")) {
+//                    System.out.println(i + ": vol " + link.text());
+                    vol = curr.substring(curr.indexOf("e") + 2);
+//                    System.out.println(vol);
+                } else if (curr.contains("EDT") && curr.contains("At")) {
+//                    System.out.println(i + ": price, ch, pch" + link.text());
+                    price = curr.substring(0,
+                            Math.max(curr.indexOf("+"), curr.indexOf("-")));
+                    ch = curr.substring(
+                            Math.max(curr.indexOf("+"), curr.indexOf("-")),
+                            curr.indexOf("(") - 1);
+                    pch = curr.substring(curr.indexOf("(") + 1,
+                            curr.indexOf(")"));
+//                    System.out.println(price + ", " + ch + ", " + pch);
+                }
+//                System.out.println(link.text());
+//                i++;
+//                Elements children = link.getAllElements();
+//                for (Element child : children) {
+//                    System.out.println(i + ": " + child);
+//                    i++;
+//                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error finding reference stock " + e);
+            e.printStackTrace(System.out);
+        }
+        if (cap.equals("N/A") || price.equals("N/A")) {
+//            System.out.println("Error null Error null " + cap + price);
+            return null;
+        }
+        Stock uStock = new Stock(ticker, price, pch, ch, vol, cap, p2e, eps);
+//        System.out.println(uStock.toString());
+        return uStock;
+    }
+
+    public Stock getStock() {
+        return uStock;
+    }
+
+    public ArrayList<Stock> getStockData(String type) {
         ArrayList<Stock> stocks = new ArrayList<Stock>();
 
         try {
             Document doc = Jsoup.connect(this.url.get(type)).get();
 //            "tr.tv-data-table__row tv-data-table__stroke tv-screener-table__result-row"
-            Elements links = doc.select(".tv-data-table__row.tv-data-table__stroke.tv-screener-table__result-row");
+            Elements links = doc.select(
+                    ".tv-data-table__row.tv-data-table__stroke.tv-screener-table__result-row");
+            if (type.equals("Technology")) {
+                doc = Jsoup.connect(this.url.get("Technology2")).get();
+                links.addAll(doc.select(
+                        ".tv-data-table__row.tv-data-table__stroke.tv-screener-table__result-row"));
+            }
 //            Elements links = doc
 //                    .select("div.tv-screener-table__symbol-right-part");
 
@@ -47,10 +143,10 @@ public class Data {
                 Elements data = link.getAllElements();
                 stocks.add(new Stock(data));
                 /*
-                 * 0 - all 1-3 - full name 4 - stock name 5 - company name 6 - price 7 - percent
-                 * change 8 - change 9-10 - recommendation 11 - volume 12 - market cap 13 -
-                 * Price-to-Earnings Ratio 14 - Earning Per Share 15 - # of employees 16-18 -
-                 * Sector
+                 * 0 - all 1-3 - full name 4 - stock name 5 - company name 6 -
+                 * price 7 - percent change 8 - change 9-10 - recommendation 11
+                 * - volume 12 - market cap 13 - Price-to-Earnings Ratio 14 -
+                 * Earning Per Share 15 - # of employees 16-18 - Sector
                  */
 
 //                int i = 0;
--- src/data/Stock.java
+++ src/data/Stock.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.util.ArrayList;
+
 import org.jsoup.select.Elements;
 
 public class Stock {
@@ -17,10 +19,10 @@ public class Stock {
     private String employees;
     private String sec;
     /*
-     * 0 - all 1-3 - full name 4 - stock name 5 - company name 6 - price 7 - percent
-     * change 8 - change 9-10 - recommendation 11 - volume 12 - market cap 13 -
-     * Price-to-Earnings Ratio 14 - Earning Per Share 15 - # of employees 16-18 -
-     * Sector
+     * 0 - all 1-3 - full name 4 - stock name 5 - company name 6 - price 7 -
+     * percent change 8 - change 9-10 - recommendation 11 - volume 12 - market
+     * cap 13 - Price-to-Earnings Ratio 14 - Earning Per Share 15 - # of
+     * employees 16-18 - Sector
      */
 
     public Stock(Elements data) {
@@ -36,6 +38,22 @@ public class Stock {
         this.eps = data.get(14).text();
         this.employees = data.get(15).text();
         this.sec = data.get(16).text();
+    }
+
+    public Stock(String s, String p, String pChange, String change, String vol,
+            String cap, String p2e, String eps) {
+        this.stock = s;
+        this.price = p;
+        this.pChange = pChange;
+        this.change = change;
+        this.vol = vol;
+        this.cap = cap;
+        this.p2e = p2e;
+        this.eps = eps;
+        this.company = "N/A";
+        this.employees = "N/A";
+        this.sec = "N/A";
+        this.rec = "N/A";
     }
 
     public String getStockName() {
@@ -86,11 +104,90 @@ public class Stock {
         return this.sec;
     }
 
+    public double toNumericalValue(String input) {
+        double output;
+        double factor = 1.0;
+        if (input.contains("K")) {
+            factor = 1000.0;
+        }
+        if (input.contains("M")) {
+            factor = 1000000.0;
+        }
+        if (input.contains("B")) {
+            factor = 1000000000.0;
+        }
+        if (input.contains("T")) {
+            factor = 1000000000000.0;
+        }
+        String clean = input.replaceAll("[^\\d.-]", "");
+        try {
+            output = Double.parseDouble(clean);
+            output = output * factor;
+        } catch (Exception e) {
+//            System.out.println(
+//                    this.stock + ": Error parsing " + input + "\n" + e);
+            output = 0.0;
+        }
+//        System.out.println(this.stock + ": Input\t" + input);
+//        System.out.println(this.stock + ": Cleaned\t" + clean);
+//        System.out.println(this.stock + ": Final\t" + output);
+        return output;
+    }
+
+    public ArrayList<Double> toVector() {
+        ArrayList<Double> s = new ArrayList<Double>();
+//        private String price;
+        s.add(this.toNumericalValue(this.price));
+//        private String pChange;
+        s.add(this.toNumericalValue(this.pChange));
+//        private String change;
+        s.add(this.toNumericalValue(this.change));
+//        private String rec;
+//        private String vol;
+        s.add(this.toNumericalValue(this.vol));
+//        private String cap;
+        s.add(this.toNumericalValue(this.cap));
+//        private String p2e;
+        s.add(this.toNumericalValue(this.p2e));
+//        private String eps;
+        s.add(this.toNumericalValue(this.eps));
+//        private String employees;
+//        private String sec;
+        return s;
+    }
+
+    public static double getDotProduct(ArrayList<Double> v1,
+            ArrayList<Double> v2) {
+        double product = 0;
+        for (int i = 0; i < v1.size(); i++) {
+            product += v1.get(i) * v2.get(i);
+        }
+        return product;
+    }
+
+    public static double getMagnitude(ArrayList<Double> v1) {
+        double mag = 0;
+        for (Double d : v1) {
+            mag += d * d;
+        }
+        return Math.sqrt(mag);
+    }
+
+    public static double getCosineSimilarity(ArrayList<Double> v1, Stock s2) {
+        ArrayList<Double> v2 = s2.toVector();
+        double m1 = getMagnitude(v1);
+        double m2 = getMagnitude(v2);
+        return getDotProduct(v1, v2) / (m1 * m2);
+    }
+
     public String toString() {
-        return "SECTOR:\t\t" + this.sec + "\n" + "STOCK:\t\t" + this.stock + "\n" + "COMPANY:\t" + this.company + "\n"
-                + "PRICE:\t\t" + this.price + "\n" + "CHANGE:\t\t" + this.change + "\n" + "% CHANGE:\t" + this.pChange
-                + "\n" + "VOLUME:\t\t" + this.vol + "\n" + "Price/Earnings:\t" + this.p2e + "\n" + "Earnings/Share:\t"
-                + this.eps + "\n" + "MARKET CAP:\t" + this.cap + "\n" + "# of EMPLOYEES:\t" + this.employees + "\n"
+        return "SECTOR:\t\t" + this.sec + "\n" + "STOCK:\t\t" + this.stock
+                + "\n" + "COMPANY:\t\t" + this.company + "\n" + "PRICE:\t\t"
+                + this.price + "\n" + "CHANGE:\t\t" + this.change + "\n"
+                + "% CHANGE:\t\t" + this.pChange + "\n" + "VOLUME:\t\t"
+                + this.vol + "\n" + "Price/Earnings:\t\t" + this.p2e + "\n"
+                + "Earnings/Share:\t" + this.eps + "\n" + "MARKET CAP:\t\t"
+                + this.cap + "\n" + "# of EMPLOYEES:\t" + this.employees + "\n"
                 + "RECOMMENDATION:\t" + this.rec + "\n";
     }
 
