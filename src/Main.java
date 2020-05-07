a69140d2d75e291e342f1500eaa101b72de2cde1
MannyKuflik <39571252+MannyKuflik@users.noreply.github.com>
2020-05-07 17:34:08 -0400

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,12 @@
+package main;
 
-import java.util.ArrayList;
-
-import data.Data;
-import data.Stock;
 import ui.GUI;
 
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Data d = new Data();
-        String[] types = { "Large Cap", "Gainers", "Losers", "Active", "Volatile", "Overbought", "Oversold" };
-        ArrayList<Stock> stocks = d.getStockData(types[1]);
-        for (Stock s : stocks) {
-            System.out.println(s.toString());
-        }
         GUI ui = new GUI();
-        System.out.println("good rep");
+        System.out.println("Running...");
     }
 
 }
